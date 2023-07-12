fda4603fd4dbd4737f523a60acf840ea81704a59
NTaher <noureddine_taher@yahoo.com>
2023-07-12 12:16:04 +0200

    add

--- spring-boot-server/src/main/java/com/spring/exemple/SpringBootJpaPostgresqlApplication.java
+++ spring-boot-server/src/main/java/com/spring/exemple/SpringBootJpaPostgresqlApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootJpaPostgresqlApplication {
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaPostgresqlApplication.class, args);
